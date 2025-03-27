f7e6e38e54ab63234e96c56ba862a439ab79f663
rc <rc@rcs-Mac-mini.local>
2025-03-27 18:34:59 +0000

    complete basic structure with placeholder templates for home, my claims, my wishes, other wishes

--- gifts/urls.py
+++ gifts/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
+
 from .views import home
+from .views import my_wishes
+from .views import other_wishes
+from .views import my_claims
+
+
+
 
 urlpatterns = [
     path('', home, name='home'),
-
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('wishes/', other_wishes, name='other_wishes'),
+    path('my-wishes/', my_wishes, name='my_wishes'),
+    path('my-claims/', my_claims, name='my_claims'),
 ]
--- gifts/views.py
+++ gifts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
     return render(request, 'gifts/homepage.html')
 
 
+def my_wishes(request):
+    return render(request, 'gifts/my-wishes.html')
+
+def other_wishes(request):
+    return render(request, 'gifts/other-wishes.html')
+
+def my_claims(request):
+    return render(request, 'gifts/my-claims.html')
