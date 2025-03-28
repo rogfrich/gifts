01f581e677c31a06251d4b66164390886610a14f
rc <rc@rcs-Mac-mini.local>
2025-03-28 17:17:12 +0000

    Set up login / logout capability and restrict pages based on login status

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from gifts.views import CustomLoginView, CustomLogoutView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gifts.urls')),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
--- gifts/views.py
+++ gifts/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Wish
+from django.contrib.auth.views import LoginView, LogoutView
+
+class CustomLoginView(LoginView):
+    template_name = 'gifts/login.html'
+    next_page = 'my_wishes'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'gifts/login.html'
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'gifts/homepage.html')
+    if request.user.is_authenticated:
+        return redirect('my_wishes')
+    return redirect('login')
+            
 
-
+@login_required
 def my_wishes(request):
     return render(request, 'gifts/my-wishes.html')
 
+@login_required
 def other_wishes(request):
     return render(request, 'gifts/other-wishes.html')
 
+@login_required
 def my_claims(request):
     return render(request, 'gifts/my-claims.html')
