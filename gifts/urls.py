89efd0978f3d10fb0d468e5f0067100a4fe9328e
rc <rc@rcs-Mac-mini.local>
2025-04-03 22:50:12 +0100

    add feature to create a wish

--- gifts/urls.py
+++ gifts/urls.py
@@ -5,6 +5,7 @@ from .views import home
 from .views import my_wishes
 from .views import other_wishes
 from .views import my_claims
+from .views import create_wish
 
 
 
@@ -15,4 +16,6 @@ urlpatterns = [
     path('wishes/', other_wishes, name='other_wishes'),
     path('my-wishes/', my_wishes, name='my_wishes'),
     path('my-claims/', my_claims, name='my_claims'),
+    path('create-wish/', create_wish, name='create_wish'),
+
 ]
--- gifts/views.py
+++ gifts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Wish
+from .forms import WishForm
 from django.contrib.auth.views import LoginView, LogoutView
 
 class CustomLoginView(LoginView):
@@ -33,3 +34,22 @@ def other_wishes(request):
 @login_required
 def my_claims(request):
     return render(request, 'gifts/my-claims.html')
+
+
+@login_required
+def create_wish(request):
+    if request.method == 'POST':
+        form = WishForm(request.POST)
+        if form.is_valid():
+            wish = form.save(commit=False)
+            wish.user = request.user
+            wish.save()
+            return redirect('my_wishes')
+    else:
+        form = WishForm()
+    
+    context = {
+        'form': form
+    }
+
+    return render(request, 'gifts/wish-form.html', context=context)
