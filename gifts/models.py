01e40d268d1d455de6ef3411ba8e808d46e09461
rc <rc@rcs-Mac-mini.local>
2025-04-03 23:00:48 +0100

    changed Wish model so that the detail and link fields are optional

--- gifts/models.py
+++ gifts/models.py
@@ -4,8 +4,8 @@ from django.contrib.auth.models import User
 class Wish(models.Model):  # Renamed from Gift to Wish
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishes_user')  # Updated related_name
     title = models.CharField(max_length=100)
-    detail = models.TextField()
-    link = models.URLField()
+    detail = models.TextField(null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     claimed = models.BooleanField(default=False)
     claimed_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishes_claimed_by', null=True, blank=True)  # Updated related_name
