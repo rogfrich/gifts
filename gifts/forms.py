0ec8adb679bbc28cc486e7db1627b192ec35b50a
rc <rc@rcs-Mac-mini.local>
2025-04-22 21:43:53 +0100

    add overide label to WishForm to indicate that title is mandatory

--- gifts/forms.py
+++ gifts/forms.py
@@ -5,8 +5,10 @@ class WishForm(forms.ModelForm):
     class Meta:
         model = Wish
         fields = ['title', 'detail', 'link']
+        labels = {
+            'title': 'Title *',} # Overrides the default label for the title field to add an asterisk to mark it as required
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder': 'Enter the title of your wish'}),
+            'title': forms.TextInput(attrs={'placeholder': 'Enter the title of your wish', 'required': 'required'}),
             'detail': forms.Textarea(attrs={'placeholder': 'Give a bit more detail about your wish'}),
             'link': forms.TextInput(attrs={'placeholder': 'Enter a link to your wish'}),
         }
