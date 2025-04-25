83ab9d76f56ed78bf682c8190ca813d25e1e0df8
rc <rc@rcs-Mac-mini.local>
2025-04-25 17:51:45 +0100

    change settings.py to read DB name from env var rather than hardcode it

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / os.getenv("DATABASE_NAME",'db.sqlite3'),
     }
 }
 
