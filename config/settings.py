82a133834240d51f85e06e890e33d6596c472102
rc <rc@rcs-Mac-mini.local>
2025-04-29 20:51:07 +0100

    improve layout on mobile

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.68.102']
 
 
 # Application definition
