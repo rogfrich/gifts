c8f36a8b40919c0ad598038118428b26dc7dff98
rc <rc@rcs-Mac-mini.local>
2025-05-15 22:30:25 +0100

    configure email settingsfor Mailgun

--- config/settings.py
+++ config/settings.py
@@ -14,12 +14,14 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-load_dotenv()
 
-ENVIRONMENT = os.getenv('ENVIRONMENT')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(dotenv_path=BASE_DIR / ".env")
+
+ENVIRONMENT = os.getenv('ENVIRONMENT')
 
 
 # Quick-start development settings - unsuitable for production
@@ -140,11 +142,11 @@ LOGOUT_REDIRECT_URL = 'login'
 if ENVIRONMENT == "dev":
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
-    # Populate this later when I've got Mailgun set up
-    # EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    # EMAIL_HOST = "smtp.yourprovider.com"
-    # EMAIL_PORT = 587
-    # EMAIL_USE_TLS = True
-    # EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-    # EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    pass
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "smtp.mailgun.org"
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # e.g., postmaster@yourdomain.com
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+
