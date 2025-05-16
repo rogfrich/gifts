65246a5325db9b18d1bdca77fb09f3243da34c83
rc <rc@rcs-Mac-mini.local>
2025-05-16 17:40:30 +0100

    update Mailgun settings

--- config/settings.py
+++ config/settings.py
@@ -142,11 +142,13 @@ LOGOUT_REDIRECT_URL = 'login'
 if ENVIRONMENT == "dev":
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
+
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     EMAIL_HOST = "smtp.mailgun.org"
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # e.g., postmaster@yourdomain.com
+    EMAIL_USE_SSL = False
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
     DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
